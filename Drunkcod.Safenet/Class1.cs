30c75b69671046090ac5249c56a5618dfc5e2711
Tobbe Gyllebring <tobbe@drunkcod.com>
2016-08-14 13:09:34 +0200

    Let's get this party started.

---
+++ Drunkcod.Safenet/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Drunkcod.Safenet
+{
+	public class SafenetResponse<T>
+	{
+		public HttpStatusCode StatusCode;
+		public T Result;
+		public SafenetError Error;
+	}
+
+	public class SafenetError
+	{
+		[JsonProperty("errorCode")] public int ErrorCode;
+		[JsonProperty("description")] public string Description;
+	}
+
+	public class SafenetDirectoryResponse
+	{
+		[JsonProperty("info")] public SafenetDirectoryInfo Info;
+		[JsonProperty("files")] public SafenetFileInfo[] Files;
+		[JsonProperty("subDirectories")] public object[] SubDirectories;
+	}
+
+	public class SafenetDirectoryInfo
+	{
+		[JsonProperty("name")] public string Name;
+		[JsonProperty("isPrivate")] public bool IsPrivate;
+		[JsonProperty("isVersioned")] public bool IsVersioned;
+		[JsonProperty("createdOn")] public DateTime CreatedOn;
+		[JsonProperty("modifiedOn")] public DateTime ModifiedOn;
+		[JsonProperty("metadata")] public string Metdata;
+	}
+
+	public class SafenetFileInfo
+	{
+		[JsonProperty("name")] public string Name;
+		[JsonProperty("size")] public int Size;
+		[JsonProperty("createdOn")] public DateTime CreatedOn;
+		[JsonProperty("modifiedOn")] public DateTime ModifiedOn;
+		[JsonProperty("metadata")] public string Metadata;
+	}
+
+}
---
+++ Drunkcod.Safenet/SafenetClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Drunkcod.Safenet
+{
+	public class SafenetClient
+	{
+		readonly HttpClient http;
+	
+		public SafenetClient(string apiRoot = "http://localhost:8100")
+		{
+			this.http = new HttpClient {
+				BaseAddress = new Uri(apiRoot),
+			};
+		}
+
+		public void SetToken(string token)
+		{
+			http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+		}
+
+		public async Task<SafenetResponse<SafenetDirectoryResponse>> DnsGetAsync(string service, string longName)
+		{
+			var r = await http.GetAsync($"/dns/{service}/{longName}");
+			var response = new SafenetResponse<SafenetDirectoryResponse>();
+			response.StatusCode = r.StatusCode;
+			if (r.IsSuccessStatusCode)
+				response.Result = (SafenetDirectoryResponse)JsonConvert.DeserializeObject((await r.Content.ReadAsStringAsync()), typeof(SafenetDirectoryResponse));
+			else
+				response.Error = (SafenetError)JsonConvert.DeserializeObject((await r.Content.ReadAsStringAsync()), typeof(SafenetError));
+			return response;
+		}
+	
+		HttpContent ToPayload(object obj) =>
+			new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+	}
+}
