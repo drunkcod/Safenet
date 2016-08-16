3148b11de8b545e55c1524f63efb54c1cf0ed7d4
Tobbe Gyllebring <tobbe@drunkcod.com>
2016-08-16 07:59:18 +0200

    Simulator console host added.

---
+++ SafenetSim/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Drunkcod.Safenet.Simulator;
+using Drunkcod.Safenet.Simulator.Controllers;
+using Microsoft.Owin.Hosting;
+
+namespace SafenetSim
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			var apiBaseAddress = args[0];
+			var knownTokens = new HashSet<string>();
+			var apiHost = WebApp.Start(apiBaseAddress, app => {
+				var config = new HttpConfiguration();
+				var deps = new SimpleDependencyResolver(config.DependencyResolver);
+				deps.Register<LauncherApiController>(() => new LauncherApiController(knownTokens));
+				var sim = new SafeSimStartup();
+				sim.Configure(app, config);
+				config.DependencyResolver = deps;
+			});
+
+			Console.ReadLine();
+			apiHost.Dispose();
+		}
+	}
+}
